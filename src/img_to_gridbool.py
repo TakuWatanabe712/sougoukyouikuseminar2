9527dcc5b2f6c0ba876b76413171b14230419677
TakuWatanabe <takuwatanabe712@gmail.com>
2026-01-07 15:48:56 +0900

    img_to_gribool関数を完成させました．

--- src/img_to_gridbool.py
+++ src/img_to_gridbool.py
@@ -1,42 +1,13 @@
 import cv2
-import numpy
+# import numpy
 
 #入力で受け取った画像を指定したピクセルで平均を取りT/Fの二次元配列にして返す.
 def img_to_grid(image) -> list[list[bool]]:
 
-    #必要に応じて調整.
-    cell_size_px = 10
-    threshold_ratio=1.0
-
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     h, w = gray_image.shape
 
-    # test
-    # print('h: ', h)
-    # print('w: ', w)
-
-    # rough_w = w // cell_size_px
-    # rough_h = h // cell_size_px
-
-    # test
-    cv2.imshow('image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # rough_image = cv2.resize(gray_image, (rough_w, rough_h), interpolation=cv2.INTER_AREA)
-    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
-    #test
-    # cv2.imshow('image', rough_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # mean_brightness = numpy.mean(rough_image)
-    # print(mean_brightness)
-
-    # prosessed_image = cv2.adaptiveThreshold(rough_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 0)
-    prosessed_image = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101, 0)
+    prosessed_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101, 110)
 
     cv2.imshow('image', prosessed_image)
     cv2.waitKey(0)
@@ -46,7 +17,7 @@ def img_to_grid(image) -> list[list[bool]]:
 
     for i in range(h):
         for j in range(w):
-            if prosessed_image[i][j] > 0 :
+            if prosessed_image[i][j] == 0 :
                 result[i][j] = True
             else:
                 result[i][j] = False
@@ -60,4 +31,9 @@ if __name__ == "__main__":
     lis = img_to_grid(image)
 
     for i in range(len(lis)):
-        print(lis[i])
+        for j in range(len(lis[i])):
+            if lis[i][j]==True:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
